03fea667ffa9faa137c23d828aca8918707ffe17
sonal satpute <sonal.satpute@gmail.com>
2020-04-05 17:10:11 +0530

    environment test

---
+++ src/test/java/EnvironmentTests.java
@@ -0,0 +1,9 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EnvironmentTests {
+    @Test
+    public void environment_should_work() {
+        Assert.assertTrue(true);
+    }
+}
